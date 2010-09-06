ea7da682814b04d6897b0a7efd4a3889f1cd3b59
nhm tanveeer hossain khan <hasan@hasan.local>
2010-09-07 01:35:16 +0700

    - Implemented Single Sign On Portal - Implemented Cross domain SSO transferrable javascript - Implemented JSON based logged user information exporter - Added missing files - Added GMail mailer support

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,6 @@ class UsersController < ApplicationController
   # render new.rhtml
   def new
     @user = User.new
-    render :action => '../sessions/new'
   end
  
   def create
@@ -19,7 +18,7 @@ class UsersController < ApplicationController
       flash[:notice] = "Thanks for signing up!  We're sending you an email with your activation code."
     else
       flash[:error]  = "We couldn't set up that account, sorry.  Please try again, or contact an admin (link is above)."
-      render :action => '../sessions/new'
+      render :action => :new
     end
   end
 
